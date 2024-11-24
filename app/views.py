240650a83be906de98c72d79beebbf948f6ed0f9
nagajyothi2313 <nagajyothi2313@gmail.com>
2024-11-24 21:13:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def abhinaya(request):
+    return HttpResponse('abhinaya is bad bad girl')
